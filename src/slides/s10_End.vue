bd7fa0f8d570b42882cc6b4511645815a2ccba65
Lai Weng Han <jlwhoo7@gmail.com>
2019-04-24 17:27:41 +0800

    clean up ending

--- src/slides/s10_End.vue
+++ src/slides/s10_End.vue
@@ -3,8 +3,12 @@
     <section>
       <div class="box">
         <h3>The End</h3>
-        <img data-src="img/cg_quasar.svg" class="gecko-quasar cg-p-2" />
-        <h4>Q & A</h4>
+        <img
+          style="padding-left:35px"
+          data-src="img/cg_quasar.svg"
+          class="gecko-quasar cg-p-2"
+        />
+        <h3>Q & A</h3>
       </div>
     </section>
   </section>
