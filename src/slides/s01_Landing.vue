90121ff0f0d26fcddf120488e467a213be1d0cdd
Lai Weng Han <jlwhoo7@gmail.com>
2019-04-24 19:25:40 +0800

    moved sequence of the slides

--- src/slides/s01_Landing.vue
+++ src/slides/s01_Landing.vue
@@ -4,7 +4,7 @@
       <h3>Build a full front end stack with Quasar Framework and Vue</h3>
       <img data-src="img/cg_quasar.svg" class="gecko-quasar" />
       <h5 style="margin:0;padding:0;">Johnson Lai</h5>
-      <div class="row justify-around cg-pt-3">
+      <div class="cg-pt-3">
         <span>
           Slides:
           <a
@@ -13,16 +13,6 @@
             rel="noopener noreferrer"
             class="link"
             >quasar.superoo7.com</a
-          >
-        </span>
-        <span>
-          Code:
-          <a
-            href="https://github.com/superoo7/quasar-presentation"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="link"
-            >github.com/superoo7/quasar-presentation</a
           >
         </span>
       </div>
